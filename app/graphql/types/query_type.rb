1ac5e8f761c05c97589a9e43dfa7e6c05aa4aa82
Mario Perez <mapreal19@gmail.com>
2018-11-23 09:04:14 +0100

    movie field in query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,10 +3,18 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    field :allMovies,
-          [Types::MovieType],
-          null: true,
-          description: "List of movies",
-          resolve: ->(obj, args, ctx) { Movie.all }
+    field :allMovies, [Types::MovieType], null: true, description: "List of movies"
+
+    field :movie, Types::MovieType, null: false, description: "Returns a movie" do
+      argument :id, ID, required: true
+    end
+
+    def all_movies
+      Movie.all
+    end
+
+    def movie(id:)
+      Movie.find(id)
+    end
   end
 end
